e83d02bbfa723086a255ea05d82a3a61dbead8eb
NaamaGutfroind <Hand2e6@mbjcomp.org.il>
2026-01-01 14:58:01 +0200

    Update SubCategory.ts

--- backend/src/models/SubCategory.ts
+++ backend/src/models/SubCategory.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema, Document, Model } from 'mongoose';
 export interface ISubCategory extends Document {
   category_id: string;
   name: string;
-  // _id כבר קיים ב-Document, אין צורך להגדיר שוב
+  
 }
 
 const SubCategorySchema: Schema<ISubCategory> = new Schema({
@@ -11,7 +11,7 @@ const SubCategorySchema: Schema<ISubCategory> = new Schema({
   name: { type: String, required: true }
 }, { 
   timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
-  // הסרת _id: false – עכשיו Mongoose יוצר _id אוטומטית
+  
 });
 
 const SubCategory: Model<ISubCategory> = mongoose.model<ISubCategory>('SubCategory', SubCategorySchema);
